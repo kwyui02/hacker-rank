cdb7974a453326842e8d9d064006048a96fbbded
Ashley Franchesca <kwyui02@gmail.com>
2022-12-31 20:05:02 +0800

    to-revise: code for if-else.py

---
+++ draft.py
@@ -0,0 +1,6 @@
+n = int(input())
+
+if (3 <= n):
+    print("yes")
+else:
+    print("no")
--- python-certification/if-else.py
+++ python-certification/if-else.py
@@ -6,5 +6,19 @@ import random
 import re
 import sys
 
+
+def main():
+    if (n % 2 == 0):  # if n is even
+        if (2 <= n <= 5):
+            print("Not Weird")
+        if (6 <= n <= 20):
+            print("Weird")
+        else:
+            print("Not Weird")
+    else:  # if n is odd
+        print("Weird")
+
+
 if __name__ == '__main__':
     n = int(input().strip())
+    main()
