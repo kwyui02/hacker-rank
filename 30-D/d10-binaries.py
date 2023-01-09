c0ec2cd93462cd5d4d4ef1060b8bb776d554f102
Ashley Franchesca <kwyui02@gmail.com>
2023-01-10 00:24:45 +0800

    finished d10-binaries.py

--- 30-D/d10-binaries.py
+++ 30-D/d10-binaries.py
@@ -7,5 +7,30 @@ import re
 import sys
 
 
+def toBaseTwo(n):
+    binary = ""
+    while n != 0:
+        binary_element = str(n % 2)
+        binary = binary + binary_element
+        n = n // 2
+
+    binary = binary[::-1]
+    return binary
+
+
+def maxOnes(binary):
+    split_binary = binary.split("0")
+    tally = []
+    for partition in split_binary:
+        count = partition.count("1")
+        tally.append(count)
+
+    sorted_tally = sorted(tally, reverse=True)
+    return sorted_tally[0]
+
+
 if __name__ == '__main__':
     n = int(input().strip())
+    binary = toBaseTwo(n)
+    output = maxOnes(binary)
+    print(output)
