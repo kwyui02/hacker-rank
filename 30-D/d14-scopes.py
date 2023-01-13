d3f494035e58cccb97bc2d9227328ec7cbfe7750
Ashley Franchesca <kwyui02@gmail.com>
2023-01-13 20:36:09 +0800

    finished d14-scopes.py

--- 30-D/d14-scopes.py
+++ 30-D/d14-scopes.py
@@ -1,8 +1,15 @@
 class Difference:
     def __init__(self, a):
         self.__elements = a
+        self.maximumDifference = 0
 
         # Add your code here
+
+    def computeDifference(self):
+        maximum_element = max(self.__elements)
+        minimum_element = min(self.__elements)
+
+        self.maximumDifference = maximum_element - minimum_element
 
 # End of Difference class
 
