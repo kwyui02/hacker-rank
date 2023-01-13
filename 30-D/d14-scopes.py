912213ec8f67b6bc0df690f5e1c0c919d83f54f6
Ashley Franchesca <kwyui02@gmail.com>
2023-01-13 20:12:17 +0800

    initialized d-14 scopes.py

---
+++ 30-D/d14-scopes.py
@@ -0,0 +1,16 @@
+class Difference:
+    def __init__(self, a):
+        self.__elements = a
+
+        # Add your code here
+
+# End of Difference class
+
+
+_ = input()
+a = [int(e) for e in input().split(' ')]
+
+d = Difference(a)
+d.computeDifference()
+
+print(d.maximumDifference)
